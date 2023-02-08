2533d5b2f7182f50f39df4bca026446460a4bd0d
rohantor <rohan.magar@torinit.ca>
2023-02-08 16:15:04 +0530

    added date time

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Homepage from './pages/Homepage';
 function App() {
+  const[currentDateTime,setCurrentDateTime] = useState('')
+  useEffect(() =>{
+
+    setInterval(() =>{
+
+      setCurrentDateTime(
+        new Date().toLocaleString('en-US', {
+          timeZone: 'Asia/Kolkata',
+          hour12: true,
+        })
+      )
+    },1000)
+  },[])
   return (
-    <div className="App">
+    <div className='App'>
       <Homepage></Homepage>
+      <h1 style={{position:'absolute' ,top:'0rem',right:'3rem'}}>{currentDateTime}</h1>
     </div>
-  );
+  )
 }
 
 export default App;
